82aaa801991efea323ff404cd40c12daa04fb143
zip2004zip <zip2004zip@mail.ru>
2015-12-27 20:44:03 +0300

    prosto

--- src/by/mix/Main.java
+++ src/by/mix/Main.java
@@ -12,30 +12,3 @@ public class Main {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-    public static void main(String[] args) {
-        int[] m1 = new int[100];
-        for (int i = 0; i < m1.length; i++) {
-            m1[i] = i + 1;
-        }
-        for (int i = 0; i < m1.length; i++) {
-            System.out.println(m1[i]);
-        }
-        long count = 0;
-        for (int i = 0; i < m1.length; i++) {
-            count = count + m1[i];
-        }
-        System.out.println(count);
-    }
-}*/
