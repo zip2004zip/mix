1beb5761e724f6a2fdf79325ce161748f6d5fcca
zip2004zip <zip2004zip@mail.ru>
2016-01-09 23:50:55 +0300

    main5

--- src/by/mix/Main5.java
+++ src/by/mix/Main5.java
@@ -2,9 +2,9 @@ package by.mix;
 
 import java.util.Random;
 
-// Задача 1 создали массив А и сделали копию из этого массива в массив Б, но только четные и положительные значения
+// Задача 1 посчитать процентное соотношение положительных и отрицательных чисел в массиве
 
-public class Main4 {
+public class Main5 {
     public static void main(String[] args) {
         int[] m1 = new int[10];
         Random random = new Random();
@@ -14,15 +14,23 @@ public class Main4 {
 
         // вывод массива
         for (int i = 0; i < m1.length; i++) {
-            System.out.println(i + ":1 " + m1[i]);
+            System.out.println(i + ":" + m1[i]);
         }
 
         // копирование массива и вывод только положительных и четных
         int[] m2 = new int[10];
+        //int j = 0;
         for (int i = 0; i < m2.length; i++) {
             if (m1[i] >= 0 && m1[i] % 2 == 0) {
                 m2[i] = m1[i];
-            System.out.println(i + ":2 " + m2[i]);}
+                //j++;
+            }
+            System.out.println(i + ":" + m2[i]);
         }
+
+        //вывод массива
+        //for (int i = 0; i < m2.length; i++) {
+        //System.out.println(i + ":" + m2[i]);
+        //}
     }
 }
