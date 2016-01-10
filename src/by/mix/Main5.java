1252a35b41d943a62330b8cb8f64444bc7418509
zip2004zip <zip2004zip@mail.ru>
2016-01-10 19:33:09 +0300

    main5

--- src/by/mix/Main5.java
+++ src/by/mix/Main5.java
@@ -2,35 +2,83 @@ package by.mix;
 
 import java.util.Random;
 
-// Задача 1 посчитать процентное соотношение положительных и отрицательных чисел в массиве
+// не смог вывести толко последнее число!!!!
+// Задача 2 посчитать процентное соотношение положительных и отрицательных чисел в массиве
 
 public class Main5 {
     public static void main(String[] args) {
         int[] m1 = new int[10];
+        int sumPol = 0;
+        int sumOtr = 0;
         Random random = new Random();
         for (int i = 0; i < m1.length; i++) {
-            m1[i] = -50 + random.nextInt(100);
+            m1[i] = -50 + random.nextInt(1000);
         }
 
-        // вывод массива
+        // вывод массива для себя, что бы витеть цифры
         for (int i = 0; i < m1.length; i++) {
-            System.out.println(i + ":" + m1[i]);
+            System.out.println(i + ": " + m1[i]);
         }
 
-        // копирование массива и вывод только положительных и четных
-        int[] m2 = new int[10];
-        //int j = 0;
-        for (int i = 0; i < m2.length; i++) {
-            if (m1[i] >= 0 && m1[i] % 2 == 0) {
-                m2[i] = m1[i];
-                //j++;
+        // вывод суммы положительных и отрицательных
+        for (int i = 0; i < m1.length; i++) {
+            if (m1[i] >= 0) {
+                sumPol++;
+            } else {
+                if (m1[i] < 0) {
+                    sumOtr++;
+                }
             }
-            System.out.println(i + ":" + m2[i]);
+            System.out.println("Сумма положительных чисел = " + sumPol + ", что составляет " + (sumPol * 100) / 10 + " %");
+            System.out.println("Сумма отрицательных чисел = " + sumOtr + ", что составляет " + (sumOtr * 100) / 10 + " %");
         }
-
-        //вывод массива
-        //for (int i = 0; i < m2.length; i++) {
-        //System.out.println(i + ":" + m2[i]);
-        //}
     }
 }
+
+
+
+
+
+
+
+// про год висок или нет
+/*
+ int g = 2003;
+if(g%4==0)
+{
+if(g%100==0)
+{
+if(g%400==0)
+{
+System.out.println('Високосный');
+}
+else
+{
+System.out.println('Не високосный');
+}
+}
+else
+{
+System.out.println('Високосный');
+}
+}
+else
+{
+System.out.println('Не високосный');
+}*/
+
+
+// факториал числа
+/*
+public class HelloWorld {
+    public static void main(String[] args) {
+      int f = 1;
+for(int i=1;i <= 10;i++);
+{
+f = f * i;
+}
+System.out.println(\"f=\" + f);
+}
+}
+*/
+
