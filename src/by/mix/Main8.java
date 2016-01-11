8af04397eec3383585af03046d6e6d8a1be3a2e5
zip2004zip <zip2004zip@mail.ru>
2016-01-11 23:06:23 +0300

    main7

--- src/by/mix/Main8.java
+++ src/by/mix/Main8.java
@@ -6,36 +6,49 @@ package by.mix;
 
 public class Main8 {
     public static void main(String[] args) {
-        //String[] m1 = new int[10];
-        String slova = "У меня Ничего не выходиТ";
-        int sumPr = 0;
-        int sumStr = 0;
-       // Random random = new Random();
-        for (int i = 0; i < slova.length; i++) {
-            m1[i] = -50 + random.nextInt(1000);
-        }
 
-        // вывод массива для себя, что бы витеть цифры
-        for (int i = 0; i < slova.length; i++) {
-            System.out.println(i + ": " + m1[i]);
-        }
+        //String [] pred = {"У меня Ничего не выходиТ"};
+        String predl = "У меня Ничего не выходиТ";
+        int sumpredl = predl.length();
+        int sumB = 0;
+        int sumM = 0;
+        for (int i = 0; i <= predl; i++) {
 
-        // вывод суммы положительных и отрицательных
-        for (int i = 0; i < m1.length; i++) {
-            if (m1[i] >= 0) {
-                sumPr++;
-            } else {
-                if (m1[i] < 0) {
-                    sumStr++;
+
+            // вывод массива для себя, что бы витеть цифры
+            for (int i = 0; i <= predl; i++) {
+                System.out.println(predl);
+            }
+
+            // вывод суммы больших и маленьких букв
+            for (int i = 0; i <= predl; i++) {
+                if (sumB[i] >= 'А' && sumB[i] <= 'Ю') {
+                    sumB++;
+                } else {
+                    if (sumM[i] >= 'а' && sumM[i] <= 'ю') {
+                        sumM++;
+                    }
                 }
             }
+            System.out.println("Сумма прописных букв = " + sumB + ", что составляет " + (sumB * 100) / sumpredl + " %");
+            System.out.println("Сумма строчных букв = " + sumM + ", что составляет " + (sumM * 100) / sumpredl + " %");
         }
-        System.out.println("Сумма положительных чисел = " + sumPr + ", что составляет " + (sumPr * 100) / 10 + " %");
-        System.out.println("Сумма отрицательных чисел = " + sumStr + ", что составляет " + (sumStr * 100) / 10 + " %");
     }
 }
 
 
-//http://www.cyberforum.ru/java-j2se/thread445697.html
-//http://www.inf1.info/kumir/percentage-char прописные и строчные
+// http://www.cyberforum.ru/java-j2se/thread445697.html
+// http://www.inf1.info/kumir/percentage-char прописные и строчные
 // http://pas1.ru/percentage-char
+// http://study-java.ru/uroki-java/rabota-so-strokami-v-java-osnovnye-metody-klassa-string/
+
+
+//задано 4 числа, необходимо найти минимальное
+    /*int[] numbers = {-9, 6, 0, -59};
+    int min = numbers[0];
+for(int i = 0; i < numbers.length; i++){
+        if(min>numbers[i])
+        min = numbers[i];
+        }
+
+        System.out.println(min);*/
