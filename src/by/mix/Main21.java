8f9ad9fc2368a714ebe062a482af71037171d79b
zip2004zip <zip2004zip@mail.ru>
2016-01-19 17:58:27 +0300

    main10

--- src/by/mix/Main21.java
+++ src/by/mix/Main21.java
@@ -46,3 +46,27 @@ public class Main21 {
     }
 }
 
+
+//http://study-java.ru/uroki-java/urok-12-mnogomerny-e-massivy-v-java/
+
+/*
+public class Mult {
+
+    public static void main(String[] args) {
+        // создаем двумерный массив 10 на 10
+        int[][] multiplyTab  = new int[10][10];
+
+        // цикл по первой размерности
+        for (int i = 0; i < 10; i++) {
+            // цикл по второй размерности
+            for (int j = 0; j < 10; j++) {
+                //инициализация элементов массива
+                multiplyTab[i][j] = (i+1)*(j+1);
+
+                //вывод элементов массива
+                System.out.print(multiplyTab[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}*/
