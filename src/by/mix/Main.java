dabc8c2fd23d2dc81b7b07b6108edb2cda501fa7
zip2004zip <zip2004zip@mail.ru>
2016-01-27 22:34:42 +0300

    main

--- src/by/mix/Main.java
+++ src/by/mix/Main.java
@@ -1,18 +1,65 @@
-package by.mix;
+/*Список Main:
+Main1
 
-// создали массив и в нем сделали копию этого массива
-public class Main {
-    public static void main(String[] args) {
-        int[] m1 = new int[10];
-        for (int i = 0; i < m1.length; i++) {
-            m1[i] = i+1;
-            System.out.println(i + ":" + m1[i]);
-        }
-        int[] m2 = new int[10];
-        for (int i = 0; i < m2.length; i++) {
+Main2
 
-            m2[i] = m1[i];
-            System.out.println(i + ":" + m2[i]);
-        }
-    }
-}
+Main3
+
+Main4
+
+Main5
+
+Main6
+
+Main7
+
+Main8
+
+Main9
+
+Main10
+
+Main11
+
+Main12
+
+Main13
+
+Main14
+
+Main15
+
+Main16
+
+Main17
+
+Main18
+
+Main19
+
+Main20
+
+Main21
+
+Main22
+
+Main23
+
+Main24
+
+Main25
+
+Main26
+
+Main27
+
+Main28
+Main29
+Main30
+Main31
+Main32
+Main33
+Main34
+Main35
+Main36
+Main37*/
