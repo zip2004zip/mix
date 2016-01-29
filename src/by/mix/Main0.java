4ac9eaec4336773825c8f47547249c962a840dd9
zip2004zip <zip2004zip@mail.ru>
2016-01-29 23:40:28 +0300

    main

--- src/by/mix/Main0.java
+++ src/by/mix/Main0.java
@@ -2,13 +2,28 @@ package by.mix;
 
 // тренировочный Main
 
+
+import java.io.IOException;
+
 public class Main0 {
-    public static void main(String[] args)
-            throws java.io.IOException {
-        int i;
-        System.out.println("press S to stop.");
-        for (i = 0; (char) System.in.read() != 'S'; i++)
-            System.out.println("Pass #" + i);
+    public static void main(String[] args) throws IOException {
+
+        vip();
+
+    }
+
+
+    public static void vip() throws IOException {
+        System.out.println("Введите число: ");
+
+        char e;
+
+        for (; ; ) {
+            e = (char) System.in.read();
+
+            if (e == 'q') break;
+        }
+        System.out.println("Vveli 2");
     }
 }
 
