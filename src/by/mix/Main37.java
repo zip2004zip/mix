c52cf4665ab7eafcdea974571bc320976c5a23b3
zip2004zip <zip2004zip@mail.ru>
2016-02-16 20:58:33 +0300

    main37

--- src/by/mix/Main37.java
+++ src/by/mix/Main37.java
@@ -5,19 +5,86 @@ package by.mix;
  */
 
 import by.mix.utils.MasUtils;
+
 import java.util.Scanner;
 
 public class Main37 {
+    public static int MAX = 100;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] names = new String[MAX];
+        boolean[] ids = new boolean[MAX];
+        String[] numbers = new String[MAX];
 
-        int[] m = initMas(1000);
+        while (true) {
+            System.out.println("\n- Телефонный справочник. Введите значение:");
+            System.out.println("1. Добавить запись");
+            System.out.println("2. Удалить запись");
+            System.out.println("3. Поиск записи");
+            System.out.println("4. Вывести запись");
+            System.out.println("5. Обновить завись");
+            System.out.println("6. Сортировать справочник");
+            System.out.println("0. Выход");
+            String menu = scanner.nextLine();
+            if ("0".equals(menu)) {
+                break;
+            } else if ("1".equals(menu)) {
+                dobavitFIO(names, ids, scanner, numbers);
 
-        MasUtils.printMas(m);
-        telephoneDirectory(m);
+            } else if ("4".equals(menu)) vivodSpiska(names, ids, numbers);
+            else {
+                System.out.println("Неверный пункт меню");
+            }
+        }
     }
 
-    public static int[] initMas(int max) {
+
+    private static void vivodSpiska(String[] names, boolean[] ids, String[] numbers) {
+        System.out.println("\nСписок всех контактов");
+        System.out.println("----------------------------------");
+        for (int i = 0; i < ids.length; i++) {
+            if (ids[i]) {
+                System.out.printf("| " + i + " " + "%15s", names[i] + " |");
+                System.out.printf("%15s", numbers[i]);
+                System.out.println();
+            }
+        }
+        System.out.println("----------------------------------");
+    }
+
+
+    public static void dobavitFIO(String[] names, boolean[] ids, Scanner scanner, String[] numbers) {
+        System.out.println("Добавление ФИО");
+        System.out.println("Введите ФИО");
+        String name = scanner.nextLine();
+        int id = findId(ids);
+        names[id] = name;
+        ids[id] = true;
+        System.out.println("Введите номер телефона в виде +375(__)___-__-__");
+        String number = scanner.nextLine();
+        numbers[id] = number;
+    }
+
+
+    public static int findId(boolean[] ids) {
+        for (int i = 0; i < ids.length; i++) {
+            if (!ids[i]) {
+                return i;
+            }
+        }
+        System.out.println("ERROR");
+        return -1;
+    }
+}
+
+
+
+
+
+
+
+    /*public static int[] initMas(int max) {
         int[] m = new int[max];
         for (int i = 0; i < m.length; i++) {
             m[i] += i;
@@ -34,6 +101,7 @@ public class Main37 {
         System.out.println("4. Обновить завись");
         System.out.println("5. Поиск записи");
         System.out.println("6. Сортировать справочник");
+        System.out.println("0. Выход");
         Scanner scanner = new Scanner(System.in);
         System.out.println();
         value = scanner.nextInt();
@@ -49,12 +117,11 @@ public class Main37 {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Введите количество элемннтов массива: ");
             int sizeMas = scanner.nextInt();
-            double sum = 0;
             int[] mas1 = new int[sizeMas];
+
             for (int i = 0; i < mas1.length; i++) {
                 System.out.println("Введите элемент массива mas[" + i + "]");
                 mas1[i] = scanner.nextInt();
-                sum += mas1[i];
             }
 
             System.out.println("- Введите номер телефона");
@@ -81,7 +148,7 @@ public class Main37 {
         }
         return m;
     }
-}
+}*/
 
 
 
