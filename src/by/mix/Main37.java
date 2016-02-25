d576f3082b6fe2c4d7d6a695155f3469f514947e
zip2004zip <zip2004zip@mail.ru>
2016-02-25 21:14:43 +0300

    main37

--- src/by/mix/Main37.java
+++ src/by/mix/Main37.java
@@ -4,12 +4,17 @@ package by.mix;
  * телефонный справочник
  */
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main37 {
     public static int MAX = 5;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String[] names = new String[MAX];
         boolean[] ids = new boolean[MAX];
@@ -28,9 +33,12 @@ public class Main37 {
             System.out.println("4. Вывести все контакты");
             System.out.println("5. Редактировать контакт");
             System.out.println("6. Сортировать контакты");
+            System.out.println("7. Сохранить в файл");
             System.out.println("0. Выход");
             String menu = scanner.nextLine();
             if ("0".equals(menu)) {
+                saveToFale(names, "Names");
+                saveToFale2(dayBirthdays, "Names2");
                 break;
             } else if ("1".equals(menu)) {
                 addContact(names, ids, scanner, phones, addresss, dayBirthdays, monthBirthdays, yearBirthdays);
@@ -51,6 +59,18 @@ public class Main37 {
                 System.out.println("\nНеверный пункт меню! Повторите!");
             }
         }
+    }
+
+    public static void saveToFale(String[] m, String fileName) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(fileName));
+        objectOutputStream.writeObject(m);
+        objectOutputStream.close();
+    }
+
+    public static void saveToFale2 (int[] m, String fileName) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(fileName));
+        objectOutputStream.writeObject(m);
+        objectOutputStream.close();
     }
 
     private static void showList(String[] names, boolean[] ids, String[] phones, String[] addresss, int[] dayBirthdays, int[] monthBirthdays, int[] yearBirthdays) {
@@ -144,11 +164,12 @@ public class Main37 {
             if ("0".equals(sort)) {
                 break;
             } else if ("1".equals(sort)) {
-                sortCompare(names);
+                //  sortCompare(names);
+                sortCompare2(ids, names);
 
             } else if ("2".equals(sort)) {
-                sortMas(dayBirthdays);
-
+                //  sortMas(dayBirthdays);
+                sortCompare3(ids, names, dayBirthdays, monthBirthdays, yearBirthdays);
             } else if ("3".equals(sort)) {
                 sortMas(monthBirthdays);
 
@@ -172,45 +193,106 @@ public class Main37 {
     }
 
     public static void sortMas(int[] fmas) {
-        for (int a = 1; a < fmas.length; a++)
-            for (int b = fmas.length - 1; b >= a; b--) {
-                if (fmas[b - 1] > fmas[b]) {           // если требуемый порядок следования не соблюдается, поменять элементы местами
-                    int t = fmas[b - 1];
-                    fmas[b - 1] = fmas[b];
-                    fmas[b] = t;
+        for (int i = 1; i < fmas.length; i++)
+            for (int j = fmas.length - 1; j >= i; j--) {
+                if (fmas[j - 1] > fmas[j]) {           // если требуемый порядок следования не соблюдается, поменять элементы местами
+                    int t = fmas[j - 1];
+                    fmas[j - 1] = fmas[j];
+                    fmas[j] = t;
                 }
             }
     }
 
 
     public static void sortCompare(String[] names) {
-        for (int b = 0; b < names.length; b++) {
-            for (int a = b + 1; a < names.length; a++) {
-                if (names[a].compareTo(names[b]) < 0) {    // если требуемый порядок следования не соблюдается, поменять элементы местами
-                    String t = names[b];
-                    names[b] = names[a];
-                    names[a] = t;
+        for (int i = 0; i < names.length; i++) {
+            for (int j = i + 1; j < names.length; j++) {
+                if (names[j].compareTo(names[i]) < 0) {    // если требуемый порядок следования не соблюдается, поменять элементы местами
+                    String t = names[i];
+                    names[i] = names[j];
+                    names[j] = t;
                 }
             }
         }
     }
 
-    public static void sortCompare2(String[] names,int[] dayBirthdays, int[] monthBirthdays, int[] yearBirthdays) {
-        for (int b = 0; b < names.length; b++) {
-            for (int a = b + 1; a < names.length; a++) {
-                if (names[a].compareTo(names[b]) < 0) {    // если требуемый порядок следования не соблюдается, поменять элементы местами
-                    String t = names[b];
-                    names[b] = names[a];
-                    names[a] = t;
+
+    public static void sortCompare2(boolean[] ids, String[] names) {
+        int minI = 0;
+        String[] namesSearch = new String[ids.length];
+        int k = 0;
+        for (int i = 0; i < ids.length; i++) {
+            if (ids[i]) {
+                for (int i1 = 0; i1 < ids.length; i1++) {
+                    if (ids[i1] && !findIn(names[i1], namesSearch)) {
+                        minI = i1;
+                        break;
+                    }
                 }
+
+                for (int j = 0; j < ids.length; j++) {
+                    if (ids[j] && !findIn(names[j], namesSearch) && names[j].compareTo(names[minI]) <= 0) {
+                        minI = j;
+                    }
+                }
+                namesSearch[k] = names[minI];
+                k++;
+                System.out.println(minI + " " + names[minI]);
             }
         }
+    }
+
+    public static void sortCompare3(boolean[] ids, String[] names, int[] dayBirthdays, int[] monthBirthdays, int[] yearBirthdays) {
+        int minI = 0;
+        String[] namesSearch = new String[ids.length];
+        int k = 0;
+        for (int i = 0; i < ids.length; i++) {
+            if (ids[i]) {
+                for (int i1 = 0; i1 < ids.length; i1++) {
+                    if (ids[i1] && !findIn(dateToString(yearBirthdays[i1], monthBirthdays[i1], dayBirthdays[i1]), namesSearch)) {
+                        minI = i1;
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < ids.length; j++) {
+                    if (ids[j] && !findIn(dateToString(yearBirthdays[j], monthBirthdays[j], dayBirthdays[j]), namesSearch) &&
+                            dateToString(yearBirthdays[j], monthBirthdays[j], dayBirthdays[j]).
+                                    compareTo(dateToString(yearBirthdays[minI], monthBirthdays[minI], dayBirthdays[minI])) <= 0) {
+                        minI = j;
+                    }
+                }
+                namesSearch[k] = dateToString(yearBirthdays[minI], monthBirthdays[minI], dayBirthdays[minI]);
+                k++;
+                System.out.println(minI + " " + names[minI] + " " + dateToString(yearBirthdays[minI], monthBirthdays[minI], dayBirthdays[minI]));
+            }
+        }
+    }
+
+    public static String dateToString(int dayBirthdays, int monthBirthdays, int yearBirthdays) {
+        String s = yearBirthdays + "-";
+        if (String.valueOf(monthBirthdays).length() == 1) {
+            s += "0";
+        }
+        s += monthBirthdays + "-";
+
+        if (dayBirthdays < 10) {
+            s += "0";
+        }
+        s += dayBirthdays;
+        return s;
+    }
+
+    public static boolean findIn(String s, String[] m) {
+        for (int i = 0; i < m.length; i++) {
+            if (s.equals(m[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
-
-
-
 
 
 // http://prologistic.com.ua/primer-sortirovki-s-pomoshh-yu-java-comparable-i-comparator.html
